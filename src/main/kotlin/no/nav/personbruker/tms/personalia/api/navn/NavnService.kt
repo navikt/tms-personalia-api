eb47397c24010d52d8a5d44186b6c64046c21bd3
sasoria <sergio.alberto.arevalo.soria@nav.no>
2021-12-22 09:34:21 +0100

    NOP-endring for å trigge bygg med ny latest av baseimaget

--- src/main/kotlin/no/nav/personbruker/tms/personalia/api/navn/NavnService.kt
+++ src/main/kotlin/no/nav/personbruker/tms/personalia/api/navn/NavnService.kt
@@ -1,7 +1,6 @@
 package no.nav.personbruker.tms.personalia.api.navn
 
 import no.nav.personbruker.tms.personalia.api.config.TokendingsTokenFetcher
-import no.nav.personbruker.tms.personalia.api.ident.Ident
 import no.nav.tms.token.support.tokenx.validation.user.TokenXUser
 
 class NavnService(
